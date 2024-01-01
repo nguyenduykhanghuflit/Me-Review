643d667c7fe813e8a2cd45b2c8d81b3c27561114
NguyenDuyKhang <nguyenduykhanghuflit@gmail.com>
2024-01-01 12:09:58 +0700

    build

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import App from '@/app';
+import App from './app';
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
